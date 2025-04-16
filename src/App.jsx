83e64b18fcb5e00177e52b58d2f3f773a2917064
tysion <tysion@gmail.com>
2025-04-16 12:30:18 +0545

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <>
       <div className=" h-screen w-full bg-gradient-to-r from-red-700 to-purple-900 flex items-center justify-center">
         <h2 className="text-center text-5xl text-white">
-          Portfolio Under Construction
+          Portfolio Under Construction by Mr ALANS
         </h2>
       </div>
     </>
