1651aa3b1925a153d02e00f7c0f25de9ff75fc39
tysion <tysion@gmail.com>
2025-04-16 12:21:09 +0545

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h2 className="text text-amber-500">This is my portfolio</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
