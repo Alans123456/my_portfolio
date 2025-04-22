e8a70047229fca1bf446b900f284cdbaf6779304
tysion <tysion@gmail.com>
2025-04-22 08:15:35 +0545

    update new

--- src/Component/Nav.jsx
+++ src/Component/Nav.jsx
@@ -11,9 +11,9 @@ export default function nav(props) {
         ))}
       </ul>
 
-      <button className="bg-purple-500 hover:bg-purple-600 text-white px-4 py-2 rounded">
+      {/* <button className="bg-purple-500 hover:bg-purple-600 text-white px-4 py-2 rounded">
         Login
-      </button>
+      </button> */}
     </nav>
   );
 }
